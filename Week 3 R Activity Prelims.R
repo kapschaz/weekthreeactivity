e1b4eec73191667f07e157fba687148919861bee
kapschaz <65638212+kapschaz@users.noreply.github.com>
2023-07-23 22:27:55 -0400

    Update Week 3 R Activity Prelims.R
    final task and code clean up

--- Week 3 R Activity Prelims.R
+++ Week 3 R Activity Prelims.R
@@ -6,13 +6,16 @@ getwd()
 setwd()
 
 #This brings the 'Titanic' data into my environment from Base R (Run this)
-Titanic
+#Titanic
+data(Titanic)
 
 #This code saves the Titanic data as a dataframe with a new name
 #TASK: Save the Titanic data as a dataframe with a new name that includes your first name
 #for example I would name my data AnnaTitanic
 
-KapambweTitanic <- Titanic
+#KapambweTitanic <- Titanic
+
+KapambweTitanic <- as.data.frame(Titanic) 
 
 #This code chunk allows me to see the top 2 rows of my data
 #TASK: Write the code that returns the top rows
@@ -22,13 +25,13 @@ head(KapambweTitanic, 2)
 #This code chunk shows me the internal structure 
 #Task: Write the code that shows the number of rows, columns, column names, class of columns   
 
-num_rows <- nrow(KapambweTitanic)
+numrows <- nrow(KapambweTitanic)
 
-num_cols <- ncol(KapambweTitanic)
+numcols <- ncol(KapambweTitanic)
 
-column_names <- colnames(KapambweTitanic)
+columnnames <- colnames(KapambweTitanic)
 
-column_classes <- sapply(KapambweTitanic, class)
+columnclasses <- sapply(KapambweTitanic, class)
 
 #This code chunk shows me some basic summary stats
 #Such as Min, Max, Mean. Quartiles...
@@ -41,10 +44,14 @@ summarystats <- summary(KapambweTitanic)
 #and write the code to create an object for just the Age column 
 #(these should show as values in the environment window, not as 'datasets'Data')
 
+Sexcolumn <- KapambweTitanic$Sex 
+Agecolumn <- KapambweTitanic$Age
 
 #This takes the individual objects ('values' in the Environment) and creates a dataframe
 #Once I do this, I can see the Data in the Environment 
 #Task: Write the code to create one new data.frame from the Sex and Age values you just created
+
+newdataframe <- data.frame(Sex = Sexcolumn, Age = Agecolumn)
 
 
 #This code chunk creates a subset from my dataset
@@ -52,30 +59,38 @@ summarystats <- summary(KapambweTitanic)
 #Task: Write the code to create a subset of the data where the Freq is > 25
 #If I wanted to save the subset, I would need to name it as done in the code above
 
+subsetdata <- KapambweTitanic[KapambweTitanic$Freq > 25, ]
+
 
 #This shows me how many rows are in my dataset and
 #TASK: Write the code that returns the number of rows
 
+numrows <- nrow(KapambweTitanic)
 
 #This shows me how many columns are in my dataset
 #TASK: Write the code that returns the number of columns
 
+numcols <- ncol(KapambweTitanic)
 
 #This also shows me how many rows and columns with just one command
 #TASK: Write the code that returns the number of rows and columns with just one command
-
+dim(KapambweTitanic)
 
 #This code will install the ggplot2 package
 #TASK: Write the code that installs the ggplot2 package
-  
+
+install.packages("ggplot2")
   
 #This code calls the ggplot2 package so I can use its functions
 #Task: Write the code that 'calls' the ggplot2 package
+
+library(ggplot2)
 
 #This code creates a bar chart of the number of passengers that survived 
 #and did not survive (2 bars)
 #Task: replace the dataframe name and column names to create the chart
 #(you should see the chart in the Plots window)
-ggplot(Yourdataframename, aes(columnname, columnname)) +
-  geom_col()
-
+#ggplot(Yourdataframename, aes(columnname, columnname)) +
+ # geom_col()
+ggplot(KapambweTitanic, aes(x = Survived)) +
+  geom_bar()
